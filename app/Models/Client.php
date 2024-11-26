9edfd9aac06e4e2f0b7597586803bce34eef41d5
emilsonsn <emilsonsn2@gmail.com>
2024-11-26 09:29:27 -0300

    feat: inicia projeto de gestão de prédios

---
+++ app/Models/Client.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    use HasFactory;
+
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+    public $table = 'clients';
+
+    protected $fillable = [
+        'name',
+        'cpf_cnpj',
+        'phone',
+        'email',
+        'address',
+        'city',
+        'state',
+    ];
+
+}
